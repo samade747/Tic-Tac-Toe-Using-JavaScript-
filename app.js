33e0cbc4711dde0be2bdbe2bdb02bef4a508459e
samade747 <samad.e747@gmail.com>
2023-11-19 16:37:06 +0500

    436PM 19 11 2023

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ let changeTurn = ()=>{
 
 // function for check if a win
 let checkWin = () =>{
+    let boxtexts = document.getElementsByClassName('boxtext');
     let wins = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
-
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6]
     ]
+    wins.forEach(e => {
+        if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== '')){
+            document.querySelector('.info').innerText = boxtexts[e[0]].innerText + 'won';
+        }
+    })
 }
 
 // Game Logic 
